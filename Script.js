d09cd9358b2071333924ab00b3e4ee78e237b4fc
Ali Shahraeeni <79097063+ALISH80@users.noreply.github.com>
2023-01-29 17:59:51 +0330

    Add files via upload

--- Script.js
+++ Script.js
@@ -10,6 +10,9 @@ let Cal_Btn = document.querySelectorAll(".cal-btn");
 let Toggle_Btn = document.querySelector(".toggle-btn");
 let Cal_Box = document.querySelector(".cal-box");
 let Last = document.querySelectorAll(".last");
+let Memory = document.querySelector(".Memory");
+let Record = document.querySelector(".record");
+let Second_Record = document.querySelector(".second-record");
 let MovingCircle = document.querySelector(".moving-circle");
 // let var_Item = document.querySelectorAll(".var-Item");
 
@@ -26,8 +29,9 @@ for (let i = 0; i < buttons.length; i++) {
         }
     });
 }
-
+let flag = 0;
 function MoveLeft() {
+    flag = 1;
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function () {
             buttons[i].style.background = "#2C2F34";
@@ -48,6 +52,17 @@ function MoveLeft() {
     Toggle_Btn.style.background = "#2C2F34";
     Cal_Box.style.color = "#C6C6C6";
     MovingCircle.style.background = "#2C2F34";
+    Memory.style.backgroundColor = "#697F86";
+    Calculator.style.color = "#697F86";
+    // Record.style.color = "#FFFFFF";
+    Clock.addEventListener("click", function () {
+        Clock.style.color = "#697F86";
+        Calculator.style.color = "#FFFFFF";
+    })
+    Calculator.addEventListener("click", function () {
+        Calculator.style.color = "#697F86";
+        Clock.style.color = "#FFFFFF";
+    })
     for (let i = 0; i < option.length; i++) {
         option[i].style.color = "#FFFFFF";
     }
@@ -62,8 +77,8 @@ function MoveLeft() {
 
 let sun = document.querySelector(".fa-sun");
 sun.addEventListener("click", MoveRight);
-
 function MoveRight() {
+    flag = 0;
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function () {
             buttons[i].classList.add("moving-circle");
@@ -76,14 +91,32 @@ function MoveRight() {
             }
         });
     }
+    // Record.style.color = "#75889E";
     Circle.classList.remove("Move_left");
     Circle.classList.add("Move_right");
+    Calculator.style.color="7E99B9";
+    Clock.style.color="black";
     Mode.style.backgroundColor = "#E7EBF0";
-    Clock.style.color = "black";
     divider.style.background = "linear-gradient(#B8B8B8, #E7EBF0, #FFFFFF)";
     Toggle_Btn.style.background = "#C1D0E2";
     Cal_Box.style.color = "#C1D0E2";
+    Memory.style.backgroundColor = "#C3D1E2";
     MovingCircle.style.background = "#E7EBF0";
+    if (Memory.style.display === "block"){
+        Calculator.style.color="#000000";
+        Clock.style.color="#7E99B9"
+    }else {
+        Calculator.style.color="#7E99B9";
+        Clock.style.color="#000000"
+    }
+    Clock.addEventListener("click", function () {
+        Clock.style.color = "#7E99B9";
+        Calculator.style.color = "#000000";
+    })
+    Calculator.addEventListener("click", function () {
+        Calculator.style.color = "#7E99B9";
+        Clock.style.color = "#000000";
+    })
     for (let i = 0; i < option.length; i++) {
         option[i].style.color = "#000000";
     }
@@ -121,23 +154,41 @@ for (let i = 0; i < Btns.length; i++) {
                 }
                 break;
             case "=":
-            //     // let FinalResult = document.querySelector(".result");
-            //     // let Value = CalBox.innerHTML;
-            //     // Value = Value.replaceAll("÷", "/");
-            //     // Value = Value.replaceAll("×", "*");
-            //     // FinalResult.innerHTML = eval(Value);
-                let Finaly = document.querySelector(".result");
-                window.localStorage.setItem(CalBox.textContent, Finaly.textContent);
-                let Rec = document.createElement("p");
-                let Second_Rec = document.createElement("p");
-                Rec.classList.add("record");
-                Rec.innerHTML += CalBox.textContent;
-                let Record_Box = document.querySelector(".records");
-                Record_Box.appendChild(Rec);
-                Second_Rec.classList.add("second-record");
-                Second_Rec.innerHTML += Finaly.textContent;
-                Record_Box.appendChild(Second_Rec);
+
+                function Recording() {
+                    let Finaly = document.querySelector(".result");
+                    let Rec = document.createElement("p");
+                    let Second_Rec = document.createElement("p");
+                    Rec.classList.add("record");
+                    Rec.innerHTML += CalBox.textContent;
+                    let Record_Box = document.querySelector(".records");
+                    if (flag === 1){
+                        console.log("hi");
+                        Rec.style.color="#FFFFFF";
+                    }else {
+                        Rec.style.color="#75889E";
+                    }
+                    Record_Box.appendChild(Rec);
+
+                    Second_Rec.classList.add("second-record");
+                    Second_Rec.innerHTML += Finaly.textContent;
+                    if (flag === 1){
+                        console.log("hi");
+                        Second_Rec.style.borderBottom="1px solid #FFFFFF";
+                    }else {
+                        Second_Rec.style.borderBottom="1px solid #75889E";
+                    }
+                    Record_Box.appendChild(Second_Rec);
+                    let hide = document.querySelector(".Empty");
+                    hide.style.display = "none";
+                    let Trash = document.querySelector(".fa-trash");
+                    Trash.classList.remove("Trash");
+                }
+
+                Recording();
                 break;
+
+
             default:
                 CalBox.innerHTML += txt;
                 let FinalResult = document.querySelector(".result");
@@ -184,14 +235,15 @@ for (let i = 0; i < Btns.length; i++) {
     });
 
 
-
-
 }
 Clock.addEventListener("click", Open_Memory);
 
 function Open_Memory() {
     let Memory = document.querySelector(".Memory");
     Memory.style.display = "block";
+    Clock.style.color = "#7E99B9";
+    let Calc = document.querySelector(".fa-calculator");
+    Calc.style.color = "black";
 }
 
 Calculator.addEventListener("click", Close_Memory);
@@ -199,6 +251,25 @@ Calculator.addEventListener("click", Close_Memory);
 function Close_Memory() {
     let Memory = document.querySelector(".Memory");
     Memory.style.display = "none";
+    Calculator.style.color = "#7E99B9";
+    Clock.style.color = "black";
 }
 
-console.log(CalBox.textContent[CalBox.textContent.length-1]);
+console.log(CalBox.textContent[CalBox.textContent.length - 1]);
+
+
+let Trash = document.querySelector(".fa-trash");
+Trash.addEventListener("click", Remove);
+
+function Remove() {
+    let Record_Box = document.querySelector(".records");
+    // Record_Box.innerHTML = "";
+    while (Record_Box.firstChild) {
+        Record_Box.removeChild(Record_Box.lastChild);
+    }
+    Trash.classList.add("Trash");
+    let empty = document.querySelector(".Empty");
+    empty.style.display = "block";
+}
+
+
